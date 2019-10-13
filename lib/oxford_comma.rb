4555b0346b0aa3cdb51ec022d7e5476e0d75a5a4
Jose Chavira <joeychavira2016@gmail.com>
2019-10-13 03:41:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
-  if array [0] && [1]
-    string = array.join(",")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
-  if array [2]
-    string2 = array.join("and ")
-  end
-  string
-  string2
+  array.join(", ")  
 end
